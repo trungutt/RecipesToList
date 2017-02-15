273413eabd4f67a0bef124c97a53d392e920f409
chaville <nguyennhuductrung300890@gmail.com>
2017-02-15 21:56:21 +0100

    chore(*): setup eslint, babel and webpack with flow support

---
+++ webpack.config.babel.js
@@ -0,0 +1,57 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const devtool = 'inline-source-map';
+const entry = [
+	'webpack-dev-server/client?http://localhost:8080',
+	'webpack/hot/only-dev-server',
+	path.join(__dirname, 'src/index'),
+];
+const output = {
+	path: __dirname,
+	filename: 'bundle.js',
+	publicPath: '/',
+};
+const devServer = {
+	contentBase: path.resolve(__dirname, 'src'),
+	quiet: false,
+	noInfo: false,
+	stats: {
+		assets: false,
+		colors: true,
+		version: false,
+		hash: false,
+		timings: false,
+		chunks: false,
+		chunkModules: false,
+	},
+};
+const scriptLoader = {
+	loader: 'babel-loader',
+	include: path.resolve(__dirname, 'src'),
+	exclude: /node_modules/,
+	test: /\.jsx$|\.js$/,
+};
+const fontLoader = {
+	loaders: ['url-loader'],
+	test: /\.png$/,
+};
+const plugins = [new webpack.HotModuleReplacementPlugin()];
+const resolve = {
+	extensions: ['.js'],
+};
+
+export default {
+	devtool,
+	entry,
+	output,
+	devServer,
+	module: {
+		loaders: [
+			scriptLoader,
+			fontLoader,
+		],
+	},
+	plugins,
+	resolve,
+};
